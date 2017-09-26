06bff7ffcd362d7937d8a1ad3a3ef62c4a1bf219
Curtis Rueden <ctrueden@wisc.edu>
2017-09-26 11:43:44 +0200

    Add license blurbs and project license
    Accomplished using:
        mvn license:update-file-header license:update-project-license

--- src/main/java/net/preibisch/simulation/SimulateTileStitching.java
+++ src/main/java/net/preibisch/simulation/SimulateTileStitching.java
@@ -1,3 +1,25 @@
+/*-
+ * #%L
+ * Library for simulating a multi-view acquisition including
+ * attenuation, convolution, reduced sampling and poission noise.
+ * %%
+ * Copyright (C) 2014 - 2017 Multiview Simulation developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 package net.preibisch.simulation;
 
 import java.util.Random;
